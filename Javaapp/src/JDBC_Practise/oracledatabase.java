63b97991987f98d114674c2eb3bd4e965a86e272
saurabhkawatra <saurabh.kawatra1@gmail.com>
2021-05-05 04:30:54 +0530

    oracle database practise completed. Inserted new entries to the employee table and retrieved them into list<employee>

--- Javaapp/src/JDBC_Practise/oracledatabase.java
+++ Javaapp/src/JDBC_Practise/oracledatabase.java
@@ -1,7 +1,37 @@
 package JDBC_Practise;
 import java.sql.*;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+class employee{
+	private int emp_id;
+	private String Name;
+	private int salary;
+	public int getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "employee [emp_id=" + emp_id + ", Name=" + Name + ", salary=" + salary + "]";
+	}
+}
 
 public class oracledatabase {
 
@@ -37,9 +67,9 @@ public class oracledatabase {
 			System.out.print("\nEnter the Employee Name:- ");Employeename=sc.nextLine();
 			System.out.println("\nEnter Employee's Salary:- ");temp=sc.nextLine();
 			Salary=Integer.parseInt(temp);
-			System.out.println("insert into Employee (Emp_Id,EmployeeName,Salary) values ("+emp_id+",'"+Employeename+"',"+Salary+");");
+			System.out.println("insert into Employee (Emp_Id,EmployeeName,Salary) values ("+emp_id+",'"+Employeename+"',"+Salary+")");
 			
-			stmt.executeUpdate("insert into Employee (Emp_Id,EmployeeName,Salary) values ("+emp_id+",'"+Employeename+"',"+Salary+");");
+			stmt.executeUpdate("insert into Employee (Emp_Id,EmployeeName,Salary) values ("+emp_id+",'"+Employeename+"',"+Salary+")");
 			
 			System.out.println();
 			System.out.println("\nTable Employee :-");
@@ -49,7 +79,22 @@ public class oracledatabase {
 			{
 				System.out.printf("\n%-6s %-30s %-30s",rs.getInt(1),rs.getString(2),rs.getInt(3));
 			}
-			
+			List<employee> emp_list=new ArrayList<>();
+			rs=stmt.executeQuery("Select * from employee");
+			while(rs.next())
+			{
+				employee e1=new employee();
+				e1.setEmp_id(rs.getInt(1));
+				e1.setName(rs.getString(2));
+				e1.setSalary(rs.getInt(3));
+				emp_list.add(e1);
+			}
+			System.out.println("\n\nPrinting from the list\n");
+		
+			for(employee e:emp_list)
+			{
+				System.out.println(e.toString());
+			}
 			
 		}catch(ClassNotFoundException c)
 		{
